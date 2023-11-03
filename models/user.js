f472db499b91855e26b22b652f5ae1f011b47572
Hamza1060 <muhammadhamza162003@gmail.com>
2023-11-03 17:45:56 +0100

    edit user.js

--- models/user.js
+++ models/user.js
@@ -8,18 +8,18 @@ const UserSchema = new Schema({
     Email: {
         type: String,
     },
+    LinkeIn_Token: {
+        type: String
+    },
+    Twitter_X_Token: {
+        type: String
+    },
+    Facebook_Token: {
+        type: String
+    },
     Password: {
         type: String,
     },
-    LinkeIn: {
-        type: String
-    },
-    Twitter_X: {
-        type: String
-    },
-    Facebook: {
-        type: String
-    }
 }
 ,{ timestamps: true }) 
 const User = mongoose.model('Users record', UserSchema);
