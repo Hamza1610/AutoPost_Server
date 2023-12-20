05d0e4b8d43937b435a883986554f62499a5b356
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-19 18:25:44 -0800

    restructure function algorithm

--- controllers/uploaders.js
+++ controllers/uploaders.js
@@ -3,85 +3,94 @@ const fs = require('fs');
 const FormData = require('form-data');
 const { isArray } = require('util');
 
-const Facebook_Uploader = (accessToken, message, postedImages) => {
+const postOnFacebook = async (accessToken, message, postedImages) => {
   console.log('------------------------------Facebook Uploader test-----------------------------------');
   console.log('Access Token:', accessToken, '\n' + '\n');
-  console.log('Message:', message,  '\n' + '\n');
+  console.log('Message:', message, '\n' + '\n');
   console.log('Images:', postedImages);
-  try {
-    postedImages.forEach((image) => { 
-      const form = new FormData();
-      form.append('caption', message); 
-      form.append('source', fs.createReadStream(image));
-      console.log('Testing');
-      axios.post('https://graph.facebook.com/v13.0/me/photos?access_token=' + accessToken, form, {
-        headers: form.getHeaders(),
-      })
-      .then(response => {
-        console.log('Posted to Facebook:', response.data);
-      })
-      .catch(error => {
-        console.error('Error posting to Facebook:', error);
-      });
 
-    });
+  try {
+    // Iterate over each posted image
+    await Promise.all(postedImages.map(async (image) => {
+      try {
+        if (typeof image === 'string') { // Check if the image is a string (file path)
+          const form = new FormData();
+          form.append('caption', message);
+          form.append('source', fs.createReadStream(image));
+
+          console.log('Testing');
+
+          // Post image to Facebook
+          const response = await axios.post('https://graph.facebook.com/v13.0/me/photos?access_token=' + accessToken, form, {
+            headers: form.getHeaders(),
+          });
+
+          console.log('Posted to Facebook:', response.data);
+        } else {
+          console.error('Invalid image path:', image);
+        }
+      } catch (error) {
+        console.error('Error posting to Facebook:', error);
+      }
+    }));
   } catch (error) {
     console.error('Facebook Uploader Error:', error);
   }
 };
 
 
-const Twitter_X_Uploader = (accessToken, message, postedImages) => {
+
+
+const postOnTwitter = async (accessToken, message, postedImages) => {
   console.log('Twitter Uploader test');
   console.log('Access Token:', accessToken);
   console.log('Message:', message);
   console.log('Images:', postedImages);
 
   try {
-    const mediaIds = [];
-
     // Upload each image
-    Promise.all(postedImages.map(uploadImage))
-      .then((mediaIds) => {
-        const status = `${message}\n${mediaIds.map(id => `media_ids=${id}`).join('&')}`;
+    const mediaIds = await Promise.all(postedImages.map(uploadImage));
 
-        axios.post('https://api.twitter.com/1.1/statuses/update.json', `status=${encodeURIComponent(status)}`, {
-          headers: {
-            'Authorization': 'Bearer ' + accessToken,
-          },
-        })
-          .then(response => {
-            console.log('Posted to Twitter:', response.data);
-          })
-          .catch(error => {
-            console.error('Error posting to Twitter:', error);
-          });
-      })
-      .catch(error => {
-        console.error('Error uploading image to Twitter:', error);
+    const status = `${message}\n${mediaIds.map(id => `media_ids=${id}`).join('&')}`;
+
+    // Post status to Twitter
+    const response = await axios.post('https://api.twitter.com/1.1/statuses/update.json', `status=${encodeURIComponent(status)}`, {
+      headers: {
+        'Authorization': 'Bearer ' + accessToken,
+      },
+    });
+
+    console.log('Posted to Twitter:', response.data);
+
+  } catch (error) {
+    console.error('Twitter Uploader Error:', error);
+  }
+
+  async function uploadImage(image) {
+    try {
+      const formData = new FormData();
+      formData.append('media', image.buffer, {
+        filename: image.originalname,
+        contentType: image.mimetype,
       });
 
-    function uploadImage(imagePath) {
-      const formData = new FormData();
-      formData.append('media', fs.createReadStream(imagePath));
-
-      return axios.post('https://upload.twitter.com/1.1/media/upload.json', formData, {
+      const response = await axios.post('https://upload.twitter.com/1.1/media/upload.json', formData, {
         headers: {
           'Authorization': 'Bearer ' + accessToken,
           ...formData.getHeaders(),
         },
-      })
-        .then(response => {
-          return response.data.media_id_string;
-        });
+      });
+
+      return response.data.media_id_string;
+    } catch (error) {
+      console.error('Error uploading image to Twitter:', error);
+      throw error; // rethrow the error to be caught by Promise.all
     }
-  } catch (error) {
-    console.error('Twitter Uploader Error:', error);
   }
 };
 
 
-const LinkeIn_Uploader = (accessToken, message, postedImages) => {
+const postOnLinkedIn = (accessToken, message, postedImages) => {
   console.log('LinkedIn Uploader test');
   console.log('Access Token:', accessToken);
   console.log('Message:', message);
@@ -114,7 +123,7 @@ const LinkeIn_Uploader = (accessToken, message, postedImages) => {
 };
 
 module.exports = {
-  Facebook_Uploader,
-  Twitter_X_Uploader,
-  LinkeIn_Uploader,
+  postOnFacebook,
+  postOnTwitter,
+  postOnLinkedIn,
 };
