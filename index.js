141f97892815de8239b17de7f4c3371b3d8e68b8
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-19 18:24:37 -0800

    change import names

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const env = require()
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const mongoose = require('mongoose');
 const User = require('./models/user')
 const multer = require('multer')
-const {LinkeIn_Uploader, Facebook_Uploader, Twitter_X_Uploader} = require('./controllers/uploaders');
+const {postOnFacebook, postOnTwitter, postOnLinkedIn} = require('./controllers/uploaders');
 const app = express();
 
 const storage = multer.memoryStorage(); // Use memory storage for the image
 const upload = multer({ storage });
 
-mongoose.connect(DbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(4000, ()=> console.log('Serveer running on port 4000!')))
-    .catch((err) => console.log(err));
+// mongoose.connect(DbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then((result) => app.listen(4000, ()=> console.log('Serveer running on port 4000!')))
+//     .catch((err) => console.log(err));
 
 // Middlewares
 app.use(express.urlencoded({ extended: true }))
@@ -48,48 +47,55 @@ app.post('/sign-in', (req, res) => {
         });
 })
 
-app.post('/api/post', upload.array('images', 10), (req, res) => {
-  const user = req.body.user;
+
+// index.js
+
+// ... (previous code)
+
+app.post('/api/post', upload.array('images', 10), async (req, res) => {
   const message = req.body.message;
   const postedImages = req.files;
 
+
   try {
-    // Your code for processing and posting data
-    // Process and post the text and image files to social media platforms here
-    User.findOne({ UserName: user })
-    .then((result) => {
+    // Loop through each posted image
+    for (let index = 0; index < postedImages.length; index++) {
+      const file = postedImages[index];
 
-      // Log the file information
-      if (postedImages && postedImages.length > 0) {
-        console.log('Uploaded files:');
-        postedImages.forEach((file, index) => {
+      // Assuming 'tempPath' is the temporary file path for each image
+      const tempPath = path.join(__dirname, 'temp-uploads', file.originalname);
 
-          const tempPath = path.join(__dirname, 'temp-uploads', file.originalname);
- 
-          // Create a temporary file from the in-memory data
-          fs.writeFileSync(tempPath, file.buffer);
-      
-          // Log the temporary file path
-          console.log(`File ${index + 1} path:`, tempPath);
-      
-          // Now you can pass `tempPath` to your upload function
-          Facebook_Uploader(result.Facebook_Token, message, tempPath);
-          console.log('Wanting to test');
-          // Optionally, remove the temporary file after use
-          fs.unlinkSync(tempPath);
-        });
+      // Create a temporary file from the in-memory data
+      fs.writeFileSync(tempPath, file.buffer);
+
+      // Log the temporary file path
+      console.log(`File ${index + 1} path:`, tempPath);
+
+      // Post on Twitter
+      await postOnLinkedIn('EAAMcmjzJZCGwBO1l25Ne7dq7g7bIbVKh2GfLSPvZC6ZAhUqYPGQYuZBJMtO6zY8SNQCeXh8hsDua5qUlAE1ZCWhCT1Yx8yqNwDpcjEFvQesUGuomZCc3feeGu4tJwYz5sKfXLqoTZB0m2eiMWA6zdQhNiRLsIizhvBcZBMCvJVQ7Xl9JL0raindE7NkZAuC4OL7paVPfsTldOsey1u1zCKXTS0z3ZCAhGYXNgZBlfoXGZCMyHUns4ZBrF1gZCklj0O5WOo2QZDZD', message, req.files)
+      .then((response) => { 
+        console.log('Response :', response);
+      })
+      .catch((error) => {
+        console.log('Error:', error);
       }
+      )
 
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ error: err})
-    });
+      // Optionally, remove the temporary file after use
+      fs.unlinkSync(tempPath);
+    }
 
+    // Additional code for processing and posting data
+
+    res.status(200).json({ message: 'Content posted successfully on social media.' });
   } catch (error) {
     console.error('An error occurred:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
-
 });
 
+// ... (remaining code)
+
+app.listen(4000, () => {
+  console.log('Server conected Successfully! ');
+})
